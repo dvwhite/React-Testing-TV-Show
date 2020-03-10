4d90a27a1897eb554ea83aeed37bdf3240925c70
David White <dwhite0627@gmail.com>
2020-03-10 18:41:58 -0400

    Use rerender to test changes in props passed down with Episodes

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -13,5 +13,28 @@ describe('When the Episodes component initially mounts', () => {
   it('renders without crashing', () => {
     render(<Episodes />)
   })
+
+  it('renders no images with an empty episodes prop', () => {
+    // Test if the component's array map doesn't occur (as expected)
+    const { queryAllByTestId, rerender } = render(<Episodes episodes={[]} />);
+    expect(queryAllByTestId(/episode/i)).toHaveLength(0);
+    expect(queryAllByTestId(/episode/i)).not.toHaveLength(1);
+
+    // Rerender the component with an array of episodes containing a single episode
+    const testSeason = [{
+      id: 553946,
+      image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg"},
+      season: 1,
+      number: 1,
+      name: "Chapter One: The Vanishing of Will Byers",
+      summary: "<p>Summary</p>",
+      runtime: 60
+    }];
+    rerender(< Episodes episodes={testSeason} />);
+    expect(queryAllByTestId(/episode/i)).toHaveLength(1);
+    expect(queryAllByTestId(/episode/i)).not.toHaveLength(0);
+  })
+
   cleanup();
-})
+});
+
