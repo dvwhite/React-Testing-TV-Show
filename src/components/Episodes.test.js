3aaef75be47808807172112deab44ea631e6c50a
David White <dwhite0627@gmail.com>
2020-03-10 21:37:27 -0400

    Remove unnecessary imports from Episodes.test.js

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Jest and RTL imports
-import { fireEvent, render, cleanup } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 
 // Component imports
