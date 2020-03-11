d730478965fbcea56a105b8b2aec49eb785651f4
David White <dwhite0627@gmail.com>
2020-03-10 22:36:23 -0400

    Add additional test data to Episodes.test.js

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -17,6 +17,7 @@ describe('When the Episodes component initially mounts', () => {
   it('renders no images with an empty episodes prop', () => {
     // Test if the component's array map doesn't occur (as expected)
     const { queryAllByTestId, rerender } = render(<Episodes episodes={[]} />);
+    expect(queryAllByTestId(/episode/i)).toStrictEqual([]);
     expect(queryAllByTestId(/episode/i)).toHaveLength(0);
     expect(queryAllByTestId(/episode/i)).not.toHaveLength(1);
 
@@ -26,12 +27,21 @@ describe('When the Episodes component initially mounts', () => {
       image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg"},
       season: 1,
       number: 1,
-      name: "Chapter One: The Vanishing of Will Byers",
+      name: "Mock Chapter One",
+      summary: "<p>Summary</p>",
+      runtime: 60
+    }, 
+    {
+      id: 553947,
+      image: {medium: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg"},
+      season: 1,
+      number: 2,
+      name: "Mock Chapter Two",
       summary: "<p>Summary</p>",
       runtime: 60
     }];
     rerender(< Episodes episodes={testSeason} />);
-    expect(queryAllByTestId(/episode/i)).toHaveLength(1);
+    expect(queryAllByTestId(/episode/i)).toHaveLength(2);
     expect(queryAllByTestId(/episode/i)).not.toHaveLength(0);
   })
 
